ec145ff6e474a539d7aad1c69f0efdaf03bb8d15
David Garner <dgarner@mintel.com>
2017-06-22 21:15:44 +0100

    Initial commit of client

---
+++ client.py
@@ -0,0 +1,97 @@
+from os import path
+import json
+import hashlib
+import argparse
+
+from websocket import create_connection
+
+
+class Client(object):
+    def __init__(self):
+        self.set_args()
+        self.set_auth_data()
+        try:
+            self.ws = create_connection('wss://' + self.hostname)
+            self.authenticate()
+            print('Bot connected - waiting to start game...')
+            self.play_game()
+        finally:
+            if hasattr(self, 'ws') and self.ws.connected:
+                self.ws.close()
+
+    def send(self, msg):
+        """
+        All messages to the server must be JSON-serialisable.
+        """
+        self.ws.send(json.dumps(msg))
+
+    def recv(self):
+        """
+        All messages from the server are valid JSON.
+        """
+        msg = self.ws.recv()
+        return (
+            json.loads(msg)
+            if msg
+            else msg
+        )
+
+    def set_args(self):
+        parser = argparse.ArgumentParser(
+            description='Python Battlebot Client'
+        )
+        parser.add_argument(
+            '--authfile',
+            type=str,
+            default='auth.json',
+            help='Use a bot credentials JSON file. (Default: auth.json)'
+        )
+        args = parser.parse_args()
+        for k, v in args._get_kwargs():
+            setattr(self, k, v)
+
+    def set_auth_data(self):
+        auth_file = path.join(
+            path.dirname(path.realpath(__file__)),
+            self.authfile,
+        )
+        with open(auth_file) as f:
+            data = json.loads(f.read())
+        for k, v in data.items():
+            setattr(self, k, v)
+
+    def authenticate(self):
+        # The first message from the server is always "salt". Append this to
+        # the bot's pass_hash, encrypt it with sha256, and then send a "login"
+        # message to the server.
+        salt = self.recv()['salt']
+        login_hash = hashlib.sha256(
+            '{}{}'.format(self.pass_hash, salt).encode()
+        ).hexdigest()
+
+        login_message = {
+            'bot_id': self.bot_id,
+            'login_hash': login_hash,
+            'game': self.game,
+        }
+        self.send(login_message)
+
+        auth_response = self.recv()
+        if (
+            not auth_response or
+            not self.ws.connected or
+            auth_response['authentication'] != 'OK'
+        ):
+            raise Exception('Failed to connect and authenticate.')
+
+    def play_game(self):
+        """
+        Override this method to implement your bot!
+        """
+        raise NotImplementedError(
+            "Write your bot's logic here. (Or even better: extend this class.)"
+        )
+
+
+if __name__ == '__main__':
+    Client()
---
+++ register.py
@@ -0,0 +1,64 @@
+from os import path
+import json
+
+import argparse
+import requests
+
+parser = argparse.ArgumentParser(
+    description='Register the bot to the server and locally save the login credentials.'
+)
+parser.add_argument(
+    'bot_id',
+    type=str,
+    help='The name of the bot.'
+)
+parser.add_argument(
+    '--hostname',
+    type=str,
+    required=True,
+    help='The hostname of the bot server. (e.g. blunderdome-server.herokuapp.com)'
+)
+parser.add_argument(
+    '--game',
+    type=str,
+    required=True,
+    help='The name of the game.'
+)
+parser.add_argument(
+    '--owner',
+    type=str,
+    default='Anonymous',
+    help='The owner of the bot. (Default: Anonymous)'
+)
+parser.add_argument(
+    '--authfile',
+    type=str,
+    default='auth.json',
+    help='Save the bot credentials JSON to a different file. (Default: auth.json)'
+)
+
+args = parser.parse_args()
+print(args.game, args.hostname)
+
+hostname = (
+    args.hostname[:-1]
+    if args.hostname.endswith('/')
+    else args.hostname
+)
+url = 'https://{}/bots/{}'.format(hostname, args.game)
+
+response = requests.post(url, json={
+    'bot_id': args.bot_id,
+    'owner': args.owner,
+})
+if not response.ok:
+    raise Exception(response.text)
+print('Bot {} registered successfully'.format(args.bot_id))
+
+data = response.json()
+data['hostname'] = hostname
+json_file = path.join(path.dirname(path.realpath(__file__)), args.authfile)
+
+with open(json_file, 'w') as f:
+    f.write(json.dumps(data, indent=2))
+print('Bot data saved to {}'.format(json_file))
